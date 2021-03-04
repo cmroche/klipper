c193972c54a1aa7fc0c2b9eec86cad9822053d42
Clifford Roche <clifford.roche@gmail.com>
2021-03-04 02:38:14 +0000

    Adding omega commands

--- klippy/extras/palette2.py
+++ klippy/extras/palette2.py
@@ -1,8 +1,13 @@
-# Palette 2 MMU support
+# Palette 2 MMU support, Firmware 9.0.9 and newer supported only!
 #
 # Copyright (C) 2021 Clifford Roche <clifford.roche@gmail.com>
 #
 # This file may be distributed under the terms of the GNU GPLv3 license.
+
+## TODO
+## - O1 command should also send a pause request, P2 will unpause once it is ready
+## - Support file listing
+## - Support smart load requests
 
 import logging
 import os
@@ -24,6 +29,7 @@ COMMAND_CLEAR = [
         "O10 D2 D0 D0 DFFE1",
         "O10 D3 D0 D0 DFFE1",
         "O10 D4 D0 D0 D0069"]
+COMMAND_FIRMWARE = "O50"
 COMMAND_PING = "O31"
 
 HEARTBEAT_SEND = 5
@@ -69,8 +75,15 @@ class Palette2:
 
     def _reset(self):
         self.omega_algorithms = []
+        self.omega_algorithms_counter = 0
         self.omega_splices = []
+        self.omega_splices_counter = 0
         self.omega_pings = []
+        self.omega_pongs = []
+        self.omega_current_ping = None
+        self.omega_header = [None] * 9
+        self.omega_header_counter = 0
+        self.omega_last_command = ""
 
     def _check_P2(self, gcmd=None):
         if self.serial:
@@ -81,6 +94,9 @@ class Palette2:
 
     cmd_Connect_Help = ("Connect to the Palette 2")
     def cmd_Connect(self, gcmd):
+        pause_check = self.printer.lookup_object("pause_resume")
+        if not pause_check:
+            raise self.printer.command_error("Palette 2 requires [pause_resume] to work, please add it to your config!")
         if self.serial:
             gcmd.respond_info("Palette 2 serial port is already active, disconnect first")
             return
@@ -135,6 +151,7 @@ class Palette2:
                 raise self.printer.command_error(INFO_NOT_CONNECTED)
 
             self.write_queue.put(gcmd.get_commandline())
+            self.gcode.run_script("PAUSE")
 
     cmd_O9_help = ("Reset print information")
     def cmd_O9(self, gcmd):
@@ -190,9 +207,8 @@ class Palette2:
 
     def cmd_O31(self, gcmd):
         if self._check_P2(gcmd):
-            param = gcmd.get_command_parameters()[4:]
             try:
-                self.omega_pings.append(int(param))
+                self.omega_current_ping = gcmd.get_commandline()
                 logging.debug("Omega ping command: %s" %(gcmd.get_commandline()))
             except:
                 gcmd.respond_info("Incorrectly formatted ping command")
@@ -203,6 +219,101 @@ class Palette2:
     def cmd_O32(self, gcmd):
         logging.debug("Omega algorithm: %s" %(gcmd.get_commandline()))
         self.omega_algorithms.append(gcmd.get_commandline()[4:])
+
+    def cmd_P2_O20(self, params):
+        # First print, we can ignore
+        if params[0] == "D5":
+            logging.info("First print on Palette")
+            return
+
+        try:
+            n = int(params[0])
+        except:
+            logging.error("O20 command has invalid parameters")
+            return
+
+        if n == 0:
+            logging.info("Sending omega header %s" %self.omega_header_counter)
+            self.write_queue.put(self.omega_header[self.omega_header_counter])
+            self.omega_header_counter = self.omega_header_counter + 1
+        elif n == 1:
+            logging.info("Sending splice info %s" %self.omega_splices_counter)
+            splice = self.omega_splices[self.omega_splices_counter]
+            self.write_queue.put("O30 D%d D%s" %(splice[0], splice[1]))
+            self.omega_splices_counter = self.omega_splices_counter + 1
+        elif n == 2:
+            logging.info("Sending currnet ping info %s")
+            self.write_queue.put(self.omega_current_ping)
+        elif n == 4:
+            logging.info("Sending algorithm info %s" %self.omega_algorithms_counter)
+            self.write_queue.put(self.omega_algorithms[self.omega_algorithms_counter])
+            self.omega_algorithms_counter = self.omega_algorithms_counter + 1
+        elif n == 8:
+            logging.info("Resending the last command to Palette 2")
+            self.write_queue.put(self.omega_last_command)
+
+    def cmd_P2_O34(self, params):
+        if len(params) > 2:
+            percent = float(params[1][1:])
+            if params[0] == "D1":
+                number = len(self.omega_pings) + 1
+                d = { "number": number, "percent": percent }
+                logging.info("Ping %d, %d percent" %(number, percent))
+                self.omega_pings.append(d)
+            elif params[0] == "D2":
+                number = len(self.omega_pongs) + 1
+                d = { "number": number, "percent": percent }
+                logging.info("Pong %d, %d percent" %(number, percent))
+                self.omega_pongs.append(d)
+
+    def cmd_P2_O40(self, params):
+        logging.info("Resume request from Palette 2")
+        self.gcode.run_script("RESUME")
+
+    def cmd_P2_O50(self, params):
+        try:
+            fw = params[0][1:]
+            logging.info("Palette 2 firmware version %s detected" %os.fwalk)
+        except:
+            logging.error("Unable to parse firmware version")
+
+        if fw < "9.0.9":
+            raise self.printer.command_error("Palette 2 firmware version is too old, update to at least 9.0.9")
+
+    def cmd_P2_O53(self, params):
+        pass
+
+    def cmd_P2_O88(self, params):
+        logging.error("Palette 2 error detected")
+        try:
+            error = int(params[0][1:], 16)
+            logging.error("Palette 2 error code %d" %error)
+        except:
+            logging.error("Unable to parse Palette 2 error")
+
+    def cmd_P2_O97(self, params):
+        pass
+
+    def cmd_P2_O100(self, params):
+        logging.info("Pause request from Palette 2")
+        self.gcode.run_script("RESUME")
+
+    def cmd_P2_0102(self, params):
+        logging.warning("Smart lod isn't supported yet")
+
+    def cmd_P2(self, line):
+        t = line.split()
+        ocode = t[0]
+        params = t[1:]
+        params_count = len(params)
+        if params_count:
+            res = [i for i in params if i[0] == "D" or i[0] == "U"]
+            if not all(res):
+                logging.error("Omega parameters are invalid")
+                return
+
+        func = getattr(self, 'cmd_P2_' + ocode, None)
+        func(params)
 
     def _run_Read(self, serial):
         while serial.is_open:
@@ -216,6 +327,9 @@ class Palette2:
                 if text_line == COMMAND_HEARTBEAT:
                     self.heartbeat = time.time()
 
+                elif text_line[0] == "O":
+                    self.cmd_P2(text_line)
+
             # do a heartbeat check
             if self.heartbeat and self.heartbeat < (time.time() - HEARTBEAT_TIMEOUT):
                 logging.error("P2 has not responded to heartbeat, Palette will disconnect")
@@ -226,11 +340,13 @@ class Palette2:
         lastHeartbeatSend = time.time()
         self.write_queue.put("\n")
         self.write_queue.put(COMMAND_HEARTBEAT)
+        self.write_queue.put(COMMAND_FIRMWARE)
         
         while serial.is_open:
             try: 
                 text_line = self.write_queue.get(True, 0.5)
                 if text_line:
+                    self.omega_last_command = text_line
                     l = text_line.strip()
                     logging.debug("%s -> P2 (%s)" %(time.time(), l))
                     terminated_line = "%s\n" %(l)
